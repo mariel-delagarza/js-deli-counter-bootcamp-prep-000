2615da698ae43060a0faf92707afeaa8c98b74f3
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-14 18:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine (katzDeliLine) {
  for (var i = 0; i < katzDeliLine.length; i++) {
    position.push([i+1]+". " + katzDeliLine[i]);
  }
- return "The line is currently: " + position.join(",");
+ return "The line is currently: " + position.join(", ");
 }
  
  
